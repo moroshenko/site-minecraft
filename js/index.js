59d1f3fd258bad9c5eee904be7b178975484ab6b
moroshenko <gdgovno@gmail.com>
2020-02-18 20:43:06 +0200

    happy birthadcsfgdday

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.write("<button on_click=\"wat_the_fak()\">Jopa</button>");
+
+function wat_the_fak(){
+    var qi = +alert("SKOKO LET");
+    for(var i = 0; i < qi; i++){
+        document.write("Дерг за ушка<br>");
+    }
+}
