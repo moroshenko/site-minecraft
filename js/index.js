8e50d8af120601d779e7c7550cc3f4655fbbf615
moroshenko <gdgovno@gmail.com>
2020-02-18 20:51:19 +0200

    happybirtsdaf7uvgukfguiy 2.0

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 document.write("<button onclick=\"wat_the_fak()\">Jopa</button>");
+document.write("<input type=\"text\" id=\"textbox\"/>");
 
 function wat_the_fak(){
-    var qi = +alert("SKOKO LET");
+    var qi = +document.getElementById("textbox").value;
     for(var i = 0; i < qi; i++){
         document.write("Дерг за ушка<br>");
     }
