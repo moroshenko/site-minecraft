5ae06d6ff59b1fe7cf2c48e938b7edf6a879414e
moroshenko <gdgovno@gmail.com>
2020-02-18 21:20:50 +0200

    zx

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+document.write("<script src=\"https://code.jquery.com/jquery-1.11.2.min.js\" type=\"text/javascript\"></script>");
 document.write("<button onclick=\"wat_the_fak()\">Jopa</button>");
 document.write("<input type=\"text\" id=\"textbox\"/>");
 
