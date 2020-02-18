56f36348caa2fa4f723db33ad5f776d35b7aacf3
moroshenko <gdgovno@gmail.com>
2020-02-18 21:22:09 +0200

    tdhy

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ document.write("<input type=\"text\" id=\"textbox\"/>");
 function wat_the_fak(){
     var qi = +document.getElementById("textbox").value;
     for(var i = 0; i < qi; i++){
-        document.write("Дерг за ушка<br>");
+        document.write("Дерг за ушка #" + i +"<br>");
     }
 }
